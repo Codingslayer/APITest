393cdffd513291d32c36e009c5a67b207b43fb25
Codingslayer <sherrera1681@gmail.com>
2022-02-14 04:03:52 -0500

    Final Version

---
+++ APICore/Controllers/PersonaController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICore.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+        private PruebaMVCContext _db;
+
+        public PersonaController(PruebaMVCContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/<PersonaController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var lst = _db.Tblciudads.ToList();
+
+            return Ok(lst);
+        }
+
+        // POST api/<PersonaController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Tblciudad model)
+        {
+            var ciudad = new Tblciudad();
+            ciudad.Nombre = model.Nombre;
+
+            _db.Tblciudads.Add(ciudad);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        // PUT api/<PersonaController>/5
+        [HttpPut]
+        public ActionResult Put([FromBody] Models.Tblciudad model)
+        {
+            var ciudad = _db.Tblciudads.Find(model.Id);
+            ciudad.Nombre = model.Nombre;
+
+            _db.Entry(ciudad).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE api/<PersonaController>/5
+        [HttpDelete]
+        public ActionResult Delete([FromBody] Models.Tblciudad model)
+        {
+            var ciudad = _db.Tblciudads.Find(model.Id);
+            _db.Tblciudads.Remove(ciudad);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ APICore/Models/Tblpersona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace APICore.Models
+{
+    public partial class Tblpersona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int IdCiudad { get; set; }
+        public string Telefono { get; set; }
+        public string Edad { get; set; }
+        public bool Activo { get; set; }
+
+        public virtual Tblciudad IdCiudadNavigation { get; set; }
+    }
+}
